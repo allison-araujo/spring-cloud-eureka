4233ef4a8ca7b92644e01d39a3c4f40034f63e58
allisonvinicius <allisonvinicius63@gmail.com>
2023-02-22 10:44:11 -0400

    feat: initual setu eurekaserver

--- eurekaserver/src/main/java/io/github/allison/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/io/github/allison/eurekaserver/EurekaserverApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaserverApplication {
 
+System.out.println("aa")
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaserverApplication.class, args);
 	}
