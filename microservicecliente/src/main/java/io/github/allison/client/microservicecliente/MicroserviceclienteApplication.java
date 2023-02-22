af197e200ee8c884c3d4875da2023dcd257eb83a
allisonvinicius <allisonvinicius63@gmail.com>
2023-02-22 13:31:33 -0400

    feat: config microservice eureka server port

--- microservicecliente/src/main/java/io/github/allison/client/microservicecliente/MicroserviceclienteApplication.java
+++ microservicecliente/src/main/java/io/github/allison/client/microservicecliente/MicroserviceclienteApplication.java
@@ -2,8 +2,10 @@ package io.github.allison.client.microservicecliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceclienteApplication {
 
 	public static void main(String[] args) {
