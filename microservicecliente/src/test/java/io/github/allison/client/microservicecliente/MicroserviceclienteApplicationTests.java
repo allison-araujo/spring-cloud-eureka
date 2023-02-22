8a120754813817c1a8e20830683834e43c9064e3
allisonvinicius <allisonvinicius63@gmail.com>
2023-02-22 12:25:02 -0400

    feat: cliente

---
+++ microservicecliente/src/test/java/io/github/allison/client/microservicecliente/MicroserviceclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.allison.client.microservicecliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
