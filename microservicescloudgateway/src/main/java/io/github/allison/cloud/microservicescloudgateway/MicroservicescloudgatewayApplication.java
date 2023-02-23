f06ed2b6e723ca3d5d02617d9a4b1f0374cf232f
allisonvinicius <allisonvinicius63@gmail.com>
2023-02-23 15:32:12 -0400

    create: create gateway cloud and config yml consumer microservices

--- microservicescloudgateway/src/main/java/io/github/allison/cloud/microservicescloudgateway/MicroservicescloudgatewayApplication.java
+++ microservicescloudgateway/src/main/java/io/github/allison/cloud/microservicescloudgateway/MicroservicescloudgatewayApplication.java
@@ -2,12 +2,26 @@ package io.github.allison.cloud.microservicescloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class MicroservicescloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicescloudgatewayApplication.class, args);
+	}
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+					.route(r -> r.path("/client/**").uri("lb://msclient") )
+				.build();
 	}
 
 }
