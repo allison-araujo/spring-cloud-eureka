1adf7573cb291120082c3154863b737c3c36ba14
allisonvinicius <allisonvinicius63@gmail.com>
2023-02-23 14:51:32 -0400

    create: create microservices cloud gateway

---
+++ microservicescloudgateway/src/main/java/io/github/allison/cloud/microservicescloudgateway/MicroservicescloudgatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.allison.cloud.microservicescloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicescloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicescloudgatewayApplication.class, args);
+	}
+
+}
---
+++ microservicescloudgateway/src/test/java/io/github/allison/cloud/microservicescloudgateway/MicroservicescloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.allison.cloud.microservicescloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicescloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
