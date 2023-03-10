5c2ec98a34e37612e0f41790f78dd762cd0009fb
allisonvinicius <allisonvinicius63@gmail.com>
2023-03-10 14:02:36 -0400

    create: implements methods return assentment data client, situation client

--- avaliablecredit/src/main/java/io/github/microservices/avaliablecredit/application/AvaliableCreditController.java
+++ avaliablecredit/src/main/java/io/github/microservices/avaliablecredit/application/AvaliableCreditController.java
@@ -2,14 +2,13 @@ package io.github.microservices.avaliablecredit.application;
 
 
 import io.github.microservices.avaliablecredit.application.exception.DadosClientNotFoundException;
-import io.github.microservices.avaliablecredit.application.exception.ErrorCommunicationException;
+import io.github.microservices.avaliablecredit.application.exception.ErrorCommunicationMicroservicesException;
+import io.github.microservices.avaliablecredit.domain.model.DataAssessment;
+import io.github.microservices.avaliablecredit.domain.model.ReturnAssesstimentClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import io.github.microservices.avaliablecredit.domain.model.SituationClient;
 
 
@@ -32,7 +31,23 @@ public class AvaliableCreditController {
             return ResponseEntity.ok(situationClient);
         }catch (DadosClientNotFoundException e){
             return ResponseEntity.notFound().build();
-        }catch(ErrorCommunicationException e){
+        }catch(ErrorCommunicationMicroservicesException e){
+            return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+        }
+
+    }
+
+
+    @PostMapping
+    public ResponseEntity carryOutEvaluation(@RequestBody DataAssessment data){
+
+        try {
+            ReturnAssesstimentClient returnAssesstimentClient = avaliableCreditService.carryOutEvaluation(data.getCpf(), data.getIncome());
+            return ResponseEntity.ok(returnAssesstimentClient);
+
+        }catch (DadosClientNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }catch(ErrorCommunicationMicroservicesException e){
             return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
         }
 
