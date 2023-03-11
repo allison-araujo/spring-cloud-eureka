ed42345e915e12c5fae28bf5bfe2dc1410c137f2
allisonvinicius <allisonvinicius63@gmail.com>
2023-03-11 10:40:46 -0400

    create: declared assign methods communication with clientLImit

--- avaliablecredit/src/main/java/io/github/microservices/avaliablecredit/infra/clients/CardsResourceClient.java
+++ avaliablecredit/src/main/java/io/github/microservices/avaliablecredit/infra/clients/CardsResourceClient.java
@@ -1,6 +1,7 @@
 package io.github.microservices.avaliablecredit.infra.clients;
 
 
+import io.github.microservices.avaliablecredit.domain.model.Card;
 import io.github.microservices.avaliablecredit.domain.model.CardClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,13 @@ import java.util.List;
 
 @FeignClient(value = "mscards", path = "/cards")
 public interface CardsResourceClient {
+
     @GetMapping(params = "cpf")
-     ResponseEntity<List<CardClient>> getCardByClient(@RequestParam("cpf") String cpf);
+    ResponseEntity<List<CardClient>> getCardByClient(@RequestParam("cpf") String cpf);
+
+    @GetMapping(params = "income")
+    public ResponseEntity<List<Card>> getCardsIncomeLimit(@RequestParam("income") Long income);
+
+
 
 }
